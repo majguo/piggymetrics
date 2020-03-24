e490ed2c6a35df7dfd2b7fc8b218d811da31a7e7
Jianguo Ma <jiangma@microsoft.com>
2020-03-24 13:06:35 +0800

    init commit for account-service and statistics-service

---
+++ account-service/src/main/java/com/mp/piggymetrics/account/service/AccountserviceRestApplication.java
@@ -0,0 +1,11 @@
+package com.mp.piggymetrics.account.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/accounts")
+public class AccountserviceRestApplication extends Application {
+}
---
+++ statistics-service/src/main/java/com/mp/piggymetrics/statistics/service/StatisticsserviceRestApplication.java
@@ -0,0 +1,11 @@
+package com.mp.piggymetrics.statistics.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/statistics")
+public class StatisticsserviceRestApplication extends Application {
+}
