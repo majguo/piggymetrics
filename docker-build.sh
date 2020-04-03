b385ffa40a243181face5ca44c8b4f70badd0ae5
Jianguo Ma <jiangma@microsoft.com>
2020-04-04 00:40:24 +0800

    utility script to build docker images

---
+++ docker-build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mvn package
+
+docker rmi gateway:1.0
+docker rmi auth-service:1.0
+docker rmi account-service:1.0
+docker rmi statistics-service:1.0
+
+docker build -t gateway:1.0 gateway/
+docker build -t auth-service:1.0 auth-service/
+docker build -t account-service:1.0 account-service/
+docker build -t statistics-service:1.0 statistics-service/
