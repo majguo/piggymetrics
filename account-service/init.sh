d5b9ca592a607ef094c580d50d8b50674dc8f4d9
Jianguo Ma <jiangma@microsoft.com>
2020-04-03 11:24:55 +0800

    update more configurations during initilization of docker container

--- account-service/init.sh
+++ account-service/init.sh
@@ -2,6 +2,10 @@
 
 mongo_host=${MONGO_HOST:-localhost}
 mongo_port=${MONGO_PORT:-27017}
+auth_host=${AUTH_HOST:-localhost}
+auth_port=${AUTH_PORT:-9180}
+statistics_host=${STATISTICS_HOST:-localhost}
+statistics_port=${STATISTICS_PORT:-9380}
 
 pwd=$1
 warPkg=$2
@@ -10,7 +14,10 @@ cd "$pwd"
 
 jar -xf "$warPkg"
 sed -i "s/\"mongodb.host\".*/\"mongodb.host\": \"${mongo_host}:${mongo_port}\"/g" WEB-INF/classes/META-INF/jnosql.json
+sed -i "s#authServiceClient/mp-rest/uri=.*#authServiceClient/mp-rest/uri=http://${auth_host}:${auth_port}#g" WEB-INF/classes/META-INF/microprofile-config.properties
+sed -i "s#statisticsServiceClient/mp-rest/uri=.*#statisticsServiceClient/mp-rest/uri=http://${statistics_host}:${statistics_port}#g" WEB-INF/classes/META-INF/microprofile-config.properties
 jar -uf "$warPkg" WEB-INF/classes/META-INF/jnosql.json
+jar -uf "$warPkg" WEB-INF/classes/META-INF/microprofile-config.properties
 rm -rf rm -rf META-INF/ WEB-INF/
 
 /opt/ol/wlp/bin/server run defaultServer
